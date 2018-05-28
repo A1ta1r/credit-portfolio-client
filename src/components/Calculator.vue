cc1f26b43b252b81fb2912f0ce94b4100bcfd303
Mikhail Sergeev <ima1ta1r@yandex.ru>
2018-05-28 22:48:51 +0500

    Init project

---
+++ src/components/Calculator.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="calculator">
+    <input type="text" title="TextBox"/>
+    <input type="submit" title="OK"/>
+  </div>
+</template>
+
+<script>
+export default {name: 'Calculator'}
+</script>
+
+<style scoped>
+
+</style>
