6b1f7b5c3fc80cbba9e2d4b39410ad3bccc21e6a
Mikhail Sergeev <m.sergeev@xsolla.com>
2018-06-04 17:53:43 +0500

    Removed boostrap-vue for now

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="app">
+    <h1>Hello App!</h1>
+    <p>
+      <!-- use router-link component for navigation. -->
+      <!-- specify the link by passing the `to` prop. -->
+      <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
+      <router-link to="/foo">Go to Foo</router-link>
+      <router-link to="/bar">Go to Bar</router-link>
+    </p>
     <router-view/>
   </div>
 </template>
@@ -11,12 +19,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
