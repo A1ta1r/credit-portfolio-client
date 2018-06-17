6d5b3065da0ce7a0ac27d5c9524cfec7dd3ad73c
list412 <erestor412@gmail.com>
2018-06-17 20:13:30 +0500

    build

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Calculator from '@/components/Calculator'
 import Signup from '@/components/Signup'
 import Signin from '@/components/Signin'
